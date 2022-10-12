2cc07397c7219faf9f6124ce2ca7904d35745e28
Raynaldi <raynaldizoel@gmail.com>
2022-10-12 19:57:33 +0700

    Add User Preferences

--- app/src/main/java/com/example/chapter6_topic2/MainActivity.kt
+++ app/src/main/java/com/example/chapter6_topic2/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.chapter6_topic2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global
+import com.example.chapter6_topic2.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var userPrefs: UserPrefs
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userPrefs = UserPrefs(this)
+
+        binding.btnSave.setOnClickListener{
+            val name = binding.etName.text.toString()
+            val age = binding.etAge.text.toString().toInt()
+
+            GlobalScope.launch {
+                userPrefs.saveData(name,age)
+            }
+        }
+
+        binding.txtName.text = userPrefs.userName.toString()
+        binding.txtAge.text = userPrefs.userAge.toString()
+
+        binding.btnClear.setOnClickListener{
+            GlobalScope.launch {
+                userPrefs.deleteData()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/chapter6_topic2/UserPrefs.kt
+++ app/src/main/java/com/example/chapter6_topic2/UserPrefs.kt
@@ -1,4 +1,41 @@
 package com.example.chapter6_topic2
 
-class UserPrefs {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+
+class UserPrefs(private val context : Context) {
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+    val NAME = stringPreferencesKey("name")
+    val AGE = intPreferencesKey("age")
+
+    suspend fun saveData(name : String, age : Int) {
+        context.dataStore.edit {
+            it[NAME] = name
+            it[AGE] = age
+        }
+    }
+
+    val userName : kotlinx.coroutines.flow.Flow<String> = context.dataStore.data
+        .map {
+            it[NAME]?: ""
+        }
+
+    val userAge : kotlinx.coroutines.flow.Flow<Int> = context.dataStore.data
+        .map {
+            it[AGE]?: 0
+        }
+
+    suspend fun deleteData(){
+        context.dataStore.edit {
+            it.clear()
+        }
+    }
+
 }
